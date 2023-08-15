d272bab74a90bf407ad6058f34ecafd1b37c0b6c
Enzo França <Enzofran4@gmail.com>
2023-08-15 15:18:23 -0400

    quarto commit

---
+++ Jsonc/Estetica.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+
+public class Estetica
+{
+    public string Nome{ get; set; }
+
+    public string Email { get; set; }
+
+    public string Cpf { get; set; }
+
+    public string datanascimento { get; set; }
+
+    public string telefone { get; set; }
+
+    public string Endereço { get; set; }
+
+    public Estetica()
+    {
+
+    }
+    public Estetica(string Nome, string Email, string Cpf, string datanascimento, string telefone, string Endereço)
+    {
+        this.Nome = Nome;
+        this.Email = Email;
+        this.Cpf = Cpf;
+        this.datanascimento = datanascimento;
+        this.telefone = telefone;
+        this.Endereço = Endereço;
+
+    }
+
+    public string JsonSerializar(Estetica estetica)
+    {
+        return JsonConvert.SerializeObject(estetica);
+    }
+    public static Estetica Deserialize(string json)
+    {
+        return JsonConvert.DeserializeObject<Estetica>(json);
+    }
+
+}
---
+++ Jsonc/Form2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jsonc
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_cadastrar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt_cancelar_Click(object sender, EventArgs e)
+        {
+            tx_nome.Clear();
+            tx_email.Clear();
+            tx_telefone.Clear();
+            tx_nascimento.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var estetica = new Estetica();
+            estetica.Nome = tx_nome.Text;
+            estetica.Email = tx_email.Text; 
+            estetica.Cpf = tx_cpf.Text;
+            estetica.datanascimento = tx_nascimento.Text;   
+            estetica.telefone = tx_telefone.Text;
+            estetica.Endereço = tx_endereço.Text;
+            
+
+
+
+            try
+            {
+                using (StreamWriter a = new StreamWriter(@"c:/dados/arquivos.json"))
+                {
+                    a.WriteLine(estetica.JsonSerializar(estetica));
+                }
+                MessageBox.Show("Arquivo salvo");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Falha" + ex.Message);
+
+
+            }
+        }
+    }
+}
